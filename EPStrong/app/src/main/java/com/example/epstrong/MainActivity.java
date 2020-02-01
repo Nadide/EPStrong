b49eddaec0de495cf93f7de5cc9d6512ecb9042b
mbeltrandelrio <38166772+mbeltrandelrio@users.noreply.github.com>
2020-02-01 16:59:14 -0700

    ReportButton_Main
    added intent to open report page when click on report

--- EPStrong/app/src/main/java/com/example/epstrong/MainActivity.java
+++ EPStrong/app/src/main/java/com/example/epstrong/MainActivity.java
@@ -1,14 +1,34 @@
 package com.example.epstrong;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Button;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.report_button);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                openReportPage();
+            }
+        });
+    }
+    public void openReportPage(){
+        Intent open = new Intent(this, ReportPage.class);
+        startActivity(open);
+        //startActivity(new Intent(this, ReportPage.class));
     }
 }
