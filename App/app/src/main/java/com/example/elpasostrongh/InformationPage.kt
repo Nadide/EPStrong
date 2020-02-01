59ec24eb17d063e58bdd3db5271060a07304a8ed
Nadide Pasali <nadidepasali96@gmail.com>
2020-02-01 12:44:53 -0700

    First Commit

---
+++ App/app/src/main/java/com/example/elpasostrongh/InformationPage.kt
@@ -0,0 +1,12 @@
+package com.example.elpasostrongh
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class InformationPage : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_information_page)
+    }
+}
---
+++ App/app/src/main/java/com/example/elpasostrongh/ReportPage.kt
@@ -0,0 +1,12 @@
+package com.example.elpasostrongh
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ReportPage : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_report_page)
+    }
+}
