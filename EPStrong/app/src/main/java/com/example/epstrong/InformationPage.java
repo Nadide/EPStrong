7d2ffa88add9718b702ac3748a1280c4976b8669
Nadide Pasali <nadidepasali96@gmail.com>
2020-02-01 15:44:58 -0700

    Source Language Updated

---
+++ EPStrong/app/src/main/java/com/example/epstrong/InformationPage.java
@@ -0,0 +1,14 @@
+package com.example.epstrong;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InformationPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_information_page);
+    }
+}
---
+++ EPStrong/app/src/main/java/com/example/epstrong/ReportPage.java
@@ -0,0 +1,14 @@
+package com.example.epstrong;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ReportPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_report_page);
+    }
+}
