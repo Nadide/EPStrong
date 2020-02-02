11a61f61c900f5b80c71a89de6384292636bbd95
mbeltrandelrio <38166772+mbeltrandelrio@users.noreply.github.com>
2020-02-01 21:25:33 -0700

    Edits to Home page
    9:25

--- EPStrong/app/src/main/java/com/example/epstrong/MainActivity.java
+++ EPStrong/app/src/main/java/com/example/epstrong/MainActivity.java
@@ -12,6 +12,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
     private Button button;
+    private Button reportButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,13 +23,28 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
+                openInfoPage();
+            }
+        });
+
+
+        reportButton = (Button) findViewById(R.id.report_button);
+        reportButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
                 openReportPage();
             }
         });
     }
-    public void openReportPage(){
+    public void openInfoPage(){
         Intent open = new Intent(this, InformationPage.class);
         startActivity(open);
         //startActivity(new Intent(this, ReportPage.class));
+    }
+
+
+    public void openReportPage(){
+        Intent open = new Intent( this, ReportPage.class);
+        startActivity(open);
     }
 }
--- EPStrong/app/src/main/java/com/example/epstrong/ReportPage.java
+++ EPStrong/app/src/main/java/com/example/epstrong/ReportPage.java
@@ -1,14 +1,28 @@
 package com.example.epstrong;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import androidx.appcompat.app.AppCompatActivity;
+import android.app.Activity;
+
+import android.view.View;
+import android.content.DialogInterface;
+import android.widget.Button;
 
 import android.os.Bundle;
 
 public class ReportPage extends AppCompatActivity {
+
+    private Button reportButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report_page);
     }
+
+
+
+
+
 }
